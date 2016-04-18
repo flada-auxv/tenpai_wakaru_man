cc40e91685aa668f012067573cce670d48c34989
flada-auxv <aseknock@gmail.com>
2016-04-19 01:22:25 +0900

    no abbreviation

--- spec/tenpai_wakaru_man_spec.rb
+++ spec/tenpai_wakaru_man_spec.rb
@@ -2,19 +2,11 @@ require 'spec_helper'
 
 describe TenpaiWakaruMan::Detector do
   describe '.tenpai?' do
+    let(:str) { "123m222p345sSSSwPPd" }
+
     subject { TenpaiWakaruMan::Detector.tenpai?(str) }
 
-    context '省略無し' do
-      let(:str) { "123m222p345sSSSwPPd" }
-
-      it { is_expected.to be true }
-    end
-
-    context '省略あり' do
-      let(:str) { "123m222p345sSSSPP" }
-
-      it { is_expected.to be true }
-    end
+    it { is_expected.to be true }
   end
 
   describe '#head_candidates' do
