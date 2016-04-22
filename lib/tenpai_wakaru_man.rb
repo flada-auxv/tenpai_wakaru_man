541ad455501a8079605a56b96b59f93df97363c7
flada-auxv <aseknock@gmail.com>
2016-04-23 00:02:58 +0900

    tenpai_wakaru_man/tiles

--- lib/tenpai_wakaru_man.rb
+++ lib/tenpai_wakaru_man.rb
@@ -1,16 +1,9 @@
 require "tenpai_wakaru_man/version"
+require "tenpai_wakaru_man/tiles"
 
 module TenpaiWakaruMan
   class Detector
     attr_accessor :tiles
-
-    TILES = {
-      "Ew" => 0,  "Sw" => 1,  "Ww" => 2,  "Nw" => 3,
-      "Cd" => 4,  "Fd" => 5,  "Pd" => 6,
-      "1m" => 7,  "2m" => 8,  "3m" => 9,  "4m" => 10, "5m" => 11, "6m" => 12, "7m" => 13, "8m" => 14, "9m" => 15,
-      "1s" => 16, "2s" => 17, "3s" => 18, "4s" => 19, "5s" => 20, "6s" => 21, "7s" => 22, "8s" => 23, "9s" => 24,
-      "1p" => 25, "2p" => 26, "3p" => 27, "4p" => 28, "5p" => 29, "6p" => 30, "7p" => 31, "8p" => 32, "9p" => 33
-    }.freeze
 
     class << self
       def tenpai?(str)
