e4c2ac080b4b69f8b663c6d9c006e8e3dd63c00f
flada-auxv <aseknock@gmail.com>
2016-04-23 18:10:58 +0900

    refactor

--- lib/tenpai_wakaru_man/scanner.rb
+++ lib/tenpai_wakaru_man/scanner.rb
@@ -2,25 +2,29 @@ require 'tenpai_wakaru_man/hand'
 
 module TenpaiWakaruMan
   class Scanner
-    REGEXP = /((\d+[mps]?|[ESWN]+w?|[PFC]+d)[lar]?)/
+    SET_REGEXP = /((\d+[msp]|[PFC]+d|[ESWN]+w)[lar]?)/
+    SUIT_SYMBOLS = "mspdw"
+    MELDED_SYMBOLS = "lar"
 
     class << self
       def scan(tile_str)
-        tile_str.scan(REGEXP).map {|match| match.first }
+        tile_str.scan(SET_REGEXP).map {|tile_with_suite, _| tile_with_suite }
       end
 
       def split(tile_str)
         scan(tile_str).map {|tile|
-          tile.delete("mspdwlar").chars.map {|str| str + tile[/[mspdw]/] }
+          suit = tile[/[#{SUIT_SYMBOLS}]/]
+          tile.delete(SUIT_SYMBOLS + MELDED_SYMBOLS).chars.map {|str| str + suit }
         }.flatten.sort_by {|tile| TILES[tile] }
       end
 
       def parse(tile_str)
         scan(tile_str).each_with_object(Hand.new) {|tile, hand|
-          if tile[/[lar]/]
+          if tile[/[#{MELDED_SYMBOLS}]/]
             hand << Set.new(tile)
           else
-            hand << tile.delete("mspdwlar").chars.map {|str| str + tile[/[mspdw]/] }
+            suit = tile[/[#{SUIT_SYMBOLS}]/]
+            hand << tile.delete(SUIT_SYMBOLS + MELDED_SYMBOLS).chars.map {|str| str + suit }
           end
         }
       end
