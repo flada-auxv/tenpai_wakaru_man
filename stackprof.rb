883fc1070cc6ecc08b88a55471f7a8915aa117f9
flada-auxv <aseknock@gmail.com>
2016-08-18 22:53:10 +0900

    stackprof

---
+++ stackprof.rb
@@ -0,0 +1,9 @@
+# bundle exec ruby stackprof.rb
+# bundle exec stackprof tmp/stackprof-cpu-myapp.dump --text --limit 5
+
+require 'stackprof'
+require 'tenpai_wakaru_man'
+
+StackProf.run(mode: :cpu, out: 'tmp/stackprof-cpu-myapp.dump') do
+  TenpaiWakaruMan::Detector.winning?("11122233445555m")
+end
