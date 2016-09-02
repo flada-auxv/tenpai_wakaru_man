831895ffb749ca3cc18189dcc6245e7435ae1ebc
flada-auxv <aseknock@gmail.com>
2016-09-03 01:10:39 +0900

    ImcompletedMeld
    TODO: s/Meld/CompletedMeld/ and extract an abstract class as Meld?

--- lib/tenpai_wakaru_man/imcompleted_meld.rb
+++ lib/tenpai_wakaru_man/imcompleted_meld.rb
@@ -12,8 +12,10 @@ module TenpaiWakaruMan
         @msp_notation = to_msp_notation
       end
 
+      # XXX hm...?
+      return nil unless @tiles.count == 2
+
       @unique_count = @tiles.uniq.count
-      @count = @tiles.count
     end
 
     def inspect
@@ -25,11 +27,56 @@ module TenpaiWakaruMan
     end
 
     def wanting_tile
-
+      @wanting_tile =
+        if triplet?
+          [@tiles.first]
+        elsif run?
+          case run_waits_form
+          when :penchan
+            @tiles.first == "1m" ? [Tile.next(@tiles.last)] : [Tile.prev(@tiles.first)]
+          when :kanchan
+            [Tile.next(@tiles.first)]
+          when :ryanmen
+            [Tile.prev(@tiles.first), Tile.next(@tiles.last)]
+          else
+            raise('unknown form')
+          end
+        else
+          nil
+        end
     end
 
-    def pair?;    @unique_count == 1 && @count == 2 end
+    def triplet?
+      @unique_count == 1
+    end
 
+    def run?
+      return false if triplet? || !only_one_suit?
+
+      distance_of_tiles <= 2
+    end
+
+    def distance_of_tiles
+      # penchan:  [1,2], kanchan:  [1,3], ryanmen: [2,3]
+      @distance_of_tiles ||= (TILES[@tiles.first] - TILES[@tiles.last]).abs
+    end
+
+    def run_waits_form
+      @run_waits_form ||=
+        case distance_of_tiles
+        when 1; include_terminal? ? :penchan : :ryanmen
+        when 2; :kanchan
+        else nil
+        end
+    end
+
+    def include_terminal?
+      /[19]/.match?(@msp_notation)
+    end
+
+    def only_one_suit?
+      @tiles.map {|tile| tile[-1] }.uniq.count == 1
+    end
 
     private
 
