39aaa9e25cebf5ba260dc65a80ca6ba9a44ebd85
flada-auxv <aseknock@gmail.com>
2016-09-04 05:25:00 +0900

    :put_litter_in_its_place:

--- lib/tenpai_wakaru_man/hand.rb
+++ lib/tenpai_wakaru_man/hand.rb
@@ -53,7 +53,7 @@ module TenpaiWakaruMan
     def winning_hands
       detect_special_form!
 
-      @winning_hands ||= head_candidates.map {|head| dup.set_head(head) }.map {|hand| hand.detect_winning_hands }.reject(&:empty?).flatten
+      @winning_hands ||= head_candidates.map {|head| dup.set_head(head) }.map {|hand| hand.detect_winning_hands }.flatten
     end
 
     def detect_winning_hands
